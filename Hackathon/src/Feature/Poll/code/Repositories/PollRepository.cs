4778b66a73bdd80d978f75204200faf05872ea50
mhemaida <m.hemaida@live.com>
2017-03-04 13:38:38 +0200

    created

---
+++ Hackathon/src/Feature/Poll/code/Repositories/PollRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Sitecore.Feature.Poll.Repositories
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using Sitecore.Data.Items;
+
+  public static class PollRepository
+  {
+    public static IEnumerable<Item> Get([NotNull] Item item)
+    {
+      if (item == null)
+        throw new ArgumentNullException(nameof(item));
+
+      return item.GetMultiListValueItems(Templates.FaqGroup.Fields.GroupMember).Where(i => i.IsDerived(Templates.Faq.ID));
+    }
+  }
+}
